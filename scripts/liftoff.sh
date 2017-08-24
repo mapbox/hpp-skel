5c869647bf5a57d3e0183e6973603f5e1768d5e9
gretacb <carol@mapbox.com>
2017-08-24 16:58:19 -0500

    add first draft for liftoff

---
+++ scripts/liftoff.sh
@@ -0,0 +1,20 @@
+# First create new repo on GitHub
+# Then run the following script from within local hpp-skel directory:
+#
+# liftoff.sh --name <new-name> --url <new-remote-repo-url> 
+#
+
+mv ../hpp-skel $name
+rm -rf .git
+git init
+
+git add .
+git commit -m "here we go"
+git remote add origin $url
+git push -u origin master
+
+# Perhaps useful for fresh start
+# rm -rf include/hello_world
+# rm -rf include/hello_world.hpp
+# cp /dev/null CHANGELOG.md
+# cp /dev/null README.md
