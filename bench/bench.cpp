9ce7d3103bd144706301741f8a7aaf98c2b2d690
gretacb <carol@mapbox.com>
2017-09-06 18:15:06 -0500

    ran tidy

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -2,8 +2,9 @@
 #include <hello_world.hpp>
 
 static void BM_exclaim(benchmark::State& state) {
-  while (state.KeepRunning())
+  while (state.KeepRunning()) {
     std::string value = hello_world::exclaim("hello");
+}
 }
 // Register the function as a benchmark
 BENCHMARK(BM_exclaim);
