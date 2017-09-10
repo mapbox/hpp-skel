429182a1bc001723aece139706e8b0b24c64ebc5
Dane Springmeyer <dane@mapbox.com>
2017-09-10 14:42:06 -0700

    Add clant-tidy NOLINT

--- bench/run.cpp
+++ bench/run.cpp
@@ -1,7 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <hello_world.hpp>
 
-static void BM_expensive(benchmark::State& state) {
+static void BM_expensive(benchmark::State& state) { // NOLINT google-runtime-references
   if (state.thread_index == 0) {
 
   }
@@ -13,15 +13,15 @@ static void BM_expensive(benchmark::State& state) {
   }
 }
 
-static void BM_exclaim(benchmark::State& state) {
+static void BM_exclaim(benchmark::State& state) { // NOLINT google-runtime-references
   while (state.KeepRunning()) {
     std::string value = hello_world::exclaim("hello");
   }
 }
 
 int main (int argc, char *argv[]) {
-  benchmark::RegisterBenchmark("BM_exclaim", BM_exclaim)->Threads(2)->Threads(4)->Threads(8);
-  benchmark::RegisterBenchmark("BM_expensive", BM_expensive)->Threads(2)->Threads(4)->Threads(8)->Arg(1000000);
+  benchmark::RegisterBenchmark("BM_exclaim", BM_exclaim)->Threads(2)->Threads(4)->Threads(8); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
+  benchmark::RegisterBenchmark("BM_expensive", BM_expensive)->Threads(2)->Threads(4)->Threads(8)->Arg(1000000); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
  
