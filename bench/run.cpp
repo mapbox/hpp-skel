dff874ea6b7ca4b257a8af8468a3fad95e3bc4db
Carol Hansen <carol@mapbox.com>
2017-09-12 14:50:32 -0500

    Benchmark: Phase 2 (#48)
    * Add multithread benchmarking:
     - inline headers
     - collapse both benchmarks into one run.cpp
     - call binary directly from Makefile
     - add ignores to codecov
     - clang format fixes
     - Add clant-tidy NOLINT
     - anti-optimization and clobber methods

---
+++ bench/run.cpp
@@ -0,0 +1,40 @@
+#include <benchmark/benchmark.h>
+#include <hello_world.hpp>
+
+static void BM_expensive(benchmark::State& state) // NOLINT google-runtime-references
+{
+    if (state.thread_index == 0)
+    {
+        // Setup code here.
+    }
+    while (state.KeepRunning())
+    {
+        std::string value = hello_world::expensive(static_cast<std::size_t>(state.range(0)));
+        benchmark::DoNotOptimize(value.data());
+        benchmark::ClobberMemory();
+    }
+    if (state.thread_index == 0)
+    {
+        // Teardown code here.
+    }
+}
+
+static void BM_exclaim(benchmark::State& state) // NOLINT google-runtime-references
+{
+    while (state.KeepRunning())
+    {
+        std::string value = hello_world::exclaim("hello");
+        benchmark::DoNotOptimize(value.data());
+        benchmark::ClobberMemory();
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    benchmark::RegisterBenchmark("BM_exclaim", BM_exclaim)->Threads(2)->Threads(4)->Threads(8);                   // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
+    benchmark::RegisterBenchmark("BM_expensive", BM_expensive)->Threads(2)->Threads(4)->Threads(8)->Arg(1000000); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+
+    return 0;
+}
